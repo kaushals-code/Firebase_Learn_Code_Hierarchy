023e716047f4b7c23929348a633acb9b7bc4fb1d
Kaushal Singh Thakur <kaushal21gs@gamil.com>
2026-03-15 17:57:57 +0530

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, ref, onValue, remove } from "firebase/database";
 import app from "./db";
 
 
@@ -15,12 +15,8 @@ const db = getDatabase(app);
 function App() {
   const [count, setCount] = useState(0);
 
-  function putData() {
-    set(ref(db, "users/kaushal"), {
-      id: 1,
-      userName: "Kaushal Singh",
-      age: 18
-    });
+  function putData(prevData) {
+    remove(ref(db, "posts"));
   };
 
   return (
@@ -32,3 +28,17 @@ function App() {
 }
 
 export default App
+
+// Firebase notes 
+/* 
+
+set -> changes the whole informatino the the give one.
+update -> adds the changes only
+push -> Pushes the info to the db with unique id's
+onValue -> code onValue(ref(db, "users/kaushal"), (snapshot) => {
+    const dat = snapshot.val();
+    console.log(dat);
+})
+remove -> remove(ref(db, "users/kaushal"));
+
+*/
